fa8c24d124ab87aa905f5f7f33cd252e5de58da9
Annette Odingo <odingoannette@gmail.com>
2020-06-02 13:58:30 +0100

    13_file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,23 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+
+foo_file = open(r"C:\Users\HP\Documents\lambda\Python\Intro-Python-I\src\foo.txt")
+# foo_file = open("foo.txt", "r")
+read_data = foo_file.read()
+foo_file.close()
+print(read_data)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open("bar.txt", "r+")
+bar_file.write("I'm not just a pretty girl \nI can swear, I can joke \nI say what's on my mind \nIf I drink, if I smoke, I keep up with the guys \nAnd you'll see me holding up my middle finger to the world ")
+bar_file.close()
+bar_file = open("bar.txt", "r")
+read_new_data = bar_file.read()
+bar_file.close()
+print(read_new_data)
